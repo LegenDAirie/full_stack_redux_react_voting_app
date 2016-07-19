6c49f2c561fd9d085b0f00f49aeafffea8f69217
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-07-19 14:43:59 -0700

    wrote the function and test for and onclick event setting the vote

--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -22,4 +22,20 @@ describe('Voting', () => {
     expect(button[0].textContent).to.equal('movie1');
     expect(button[1].textContent).to.equal('movie2');
   });
+
+  it('invokes callback when a button is clicked', () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+
+    const component = renderIntoDocument(
+      <Voting pair={['movie1', 'movie2']}
+      vote={vote}/>
+    );
+
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+
+    expect(votedWith).to.eq('movie1');
+  });
+
 });
