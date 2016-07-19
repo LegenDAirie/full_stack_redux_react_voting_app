cf1c2d988865b75cb8de5f91bc3624c9cfaae6bd
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-07-19 15:23:05 -0700

    extracted the rendering of winner and vote into their own files

---
+++ voting-client/src/components/winner.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default React.createClass({
+  render: function() {
+    return <div className="winner">
+    winner is {this.props.winner}!
+    </div>;
+  }
+});
