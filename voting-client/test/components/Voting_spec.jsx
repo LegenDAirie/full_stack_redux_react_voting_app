0f2f4eaf746c7962226db9a44970a916ce791eee
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-07-18 19:25:43 -0700

    implemented a unit test to check if a pair of buttons were rendered

--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -1,5 +1,25 @@
 import Voting from "../../src/components/Voting";
+import React from 'React';
+import ReactDom from 'react-dom';
+import {
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithTag,
+  Simulate
+} from 'react-addons-test-utils';
+import {expect} from 'chai';
+
 
 describe('Voting', () => {
-  
+
+  it('renders a pair of buttons', () => {
+    const component = renderIntoDocument(
+      <Voting pair={["movie1", "movie2"]} />
+    );
+
+    const button = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(button.length).to.equal(2);
+    expect(button[0].textContent).to.equal('movie1');
+    expect(button[1].textContent).to.equal('movie2');
+  });
 });
