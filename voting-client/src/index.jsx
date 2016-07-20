b2a91174dac98eeddcc360c065a888740bbc03d8
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-07-19 17:01:03 -0700

    made a simple implementation of the results route

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import {Route} from 'react-router';
+import {Router, Route, hashHistory} from 'react-router';
 import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 const pair = ['movie1', 'movie2'];
 
 const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
   <Route path="/" component={Voting} />
 </Route>;
 
 ReactDom.render(
-  <Voting pair={pair} winner="Trainspotting" />,
+  <Router history={hashHistory}>{routes}</Router>,
   document.getElementById('app')
+  // <Voting pair={pair} winner="Trainspotting" />,
 );
