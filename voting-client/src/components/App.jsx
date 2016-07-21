ba7dadc8ed52daef9fa6ea411d355882e8a47563
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-07-20 17:47:53 -0700

    removed hardcoded data from app.jsx since it is no longer needed

--- voting-client/src/components/App.jsx
+++ voting-client/src/components/App.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import {List, Map} from 'immutable';
-
-const pair = List.of('movie1', 'movie2');
-const tally = Map({'movie1': 5, 'movie2': 4});
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, {
-      pair: pair,
-      tally: tally
-    });
+    return this.props.children;
   }
 });
